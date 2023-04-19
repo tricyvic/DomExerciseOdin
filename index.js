15bd195ccecbb4eb4f42f853937eb848551c7adf
Victor Muhita <muhitavictor69@gmail.com>
2023-04-19 20:44:20 +0300

    create element and style them

---
+++ index.js
@@ -0,0 +1,37 @@
+const body = document.querySelector("body")
+body.style = "text-align:center" 
+
+
+const createHeading = document.createElement("h3")
+createHeading.innerHTML = "I'am blue"
+createHeading.style = "color:blue"
+body.appendChild(createHeading)
+
+
+const createParagraphElement = document.createElement("p")
+createParagraphElement.innerText = "Hey I'm red!"
+createParagraphElement.style = "color:red;font-size:30px"
+body.appendChild(createParagraphElement)
+
+
+const createNewDiv = document.createElement("div")
+const createNewDivP = document.createElement("p")
+const createNewDivh1 = document.createElement("h1")
+const addDivH1 = createNewDiv.appendChild(createNewDivh1)
+const addDivP =createNewDiv.appendChild(createNewDivP);
+addDivH1.innerText = "Im in a div"
+addDivP.innerText = "ME TOO!"
+body.appendChild(createNewDiv)
+
+
+
+
+
+
+
+
+
+
+
+
+
